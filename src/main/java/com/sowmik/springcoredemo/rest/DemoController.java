1f7f26f7815d5441ce9dfccce8faa597514db4cd
sowmik-sec <sowmik.sec@gmail.com>
2023-10-17 10:52:13 +0600

    bean lifecycle methods

--- src/main/java/com/sowmik/springcoredemo/rest/DemoController.java
+++ src/main/java/com/sowmik/springcoredemo/rest/DemoController.java
@@ -10,24 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
-    private Coach anotherCoach;
+    //private Coach anotherCoach;
 
     //define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach, @Qualifier("cricketCoach") Coach theAnotherCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         System.out.println("In constructor: "+getClass().getSimpleName());
         myCoach = theCoach;
-        anotherCoach = theAnotherCoach;
     }
 //    @Autowired
 //    public void setCoach(Coach theCoach) { // method name can be anything
 //        myCoach = theCoach;
 //    }
 
-    @GetMapping("/check")
-    public String check() {
-        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
-    }
+//    @GetMapping("/check")
+//    public String check() {
+//        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
+//    }
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
