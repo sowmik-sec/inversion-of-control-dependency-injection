3177e1a1443522372429c651cc1e748dfe0f3a88
sowmik-sec <sowmik.sec@gmail.com>
2023-10-17 10:04:19 +0600

    qualifier in spring

--- src/main/java/com/sowmik/springcoredemo/rest/DemoController.java
+++ src/main/java/com/sowmik/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.sowmik.springcoredemo.rest;
 
 import com.sowmik.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +11,15 @@ public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
 
-    // define a constructor for dependency injection
-//    @Autowired
-//    public DemoController(Coach theCoach) {
-//        myCoach = theCoach;
-//    }
+    //define a constructor for dependency injection
     @Autowired
-    public void setCoach(Coach theCoach) { // method name can be anything
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach) {
         myCoach = theCoach;
     }
+//    @Autowired
+//    public void setCoach(Coach theCoach) { // method name can be anything
+//        myCoach = theCoach;
+//    }
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
